acced05a7431c0857dbbfd2b329dee8f04c89cc9
aluno <aluno@192.168.10.12>
2013-03-20 23:04:30 -0300

    povoador incluindo gerador e função gerarSobrenome

---
+++ Projeto Grade Curricular/src/br/unesp/igce/prograr/Aluno.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unesp.igce.prograr;
+
+/**
+ *
+ * @author aluno
+ */
+public class Aluno {
+    private String nome;
+    private Int ra, anoEntrada;
+}
